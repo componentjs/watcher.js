40aca19bed299fbea2540fff02e492d59bdcf703
Jonathan Ong <jonathanrichardong@gmail.com>
2014-04-03 17:11:01 -0700

    repurposed, and 1.0.0

--- README.md
+++ README.md
@@ -1,90 +1,36 @@
-# Watcher.js
+# Component Watcher
 
-A build watcher for component. [component(1)](https://github.com/component/component) will integrate a lot of these features once they are stable. You are expected to fork this if you want your own watcher or builder - do not expect every possible option.
-There are just too many.
+A watch utility for Component. It watches a `component.json`, then emits `resolve`, `scripts`, and `styles` events, allowing you to trigger these functions elsewhere.
 
-![](http://i.imgur.com/YHR3m0F.png)
+## API
 
-Features:
+### var watcher = watch([options])
 
-- Watches for file changes in the component's paths and automatically build.
-- Automatically installs all the dependencies.
-- Build without re-resolving.
-- Build just your JS or CSS depending on the file changed.
-- Check if your dependencies are outdated.
-- Update and/or your outdated dependencies.
+Options:
 
-Notes:
+- `root` <process.cwd()> - path where the main `component.json` is located.
+- `extensions` - extensions to look up for `scripts` and `styles`. Default `{ scripts: ['js', 'json', 'html'], styles: ['css'] }`.
+- `fields` - fields to look up for `scripts` and `styles`. Default `{ scripts: ['scripts', 'json', 'templates'], styles: ['styles'] }`.
 
-- Probably only works on UNIX.
-- Only works with local components in paths right now.
-- File builder is not yet implemented (which extensions do we watch?).
-- Implements the newer component spec, so change your `local` to `locals`.
+### watcher.start()
 
-## Usage
+Start or restart the watcher. Aliased as `watcher.restart()`. `watcher` starts automatically, so this is unnecessary.
 
-You probably want to fork this. In particular, you probably want a builder specific to an app. Fork this repo and set it as a dependency in your `package.json`. Don't bother publishing or versioning it:
+### watcher.stop()
 
-```js
-{
-  "devDependencies": {
-    "component-watcher": "jonathanong/watcher.js"
-  }
-}
-```
+Stop the watcher. Aliased as `watcher.close()`.
 
-```bash
-npm install
-```
+### Event: resolve
 
-It's now a dependency, now run it with:
+Emitted when a `component.json` file has been changed or a file has been added/removed, meaning you should re-resolve and create new builds.
 
-```
-node --harmony-generators ./node_modules/component-watcher
-```
+### Event: scripts
 
-Note that it's not a binary yet. You also have to run it with `--harmony-generators` for now.
+Emitter when a `scripts` file has changed. Meaning you can use your previous `tree` and rebuild.
 
-### config.json
+### Event: styles
 
-See the included `config.json` file. Some configurations are set in this file. You can also specify your own file with the environmental variable `COMPONENT_WATCHER_CONFIG` (suggest a better name!).
-
-Some options:
-
-- `fields` - the fields to download. By default, it downloads all the fields in the spec.
-- `extensions` - extensions to watch on a per-builder basis.
-- `options` - various options for the resolver and builders.
-
-However, not every option can be set here. For example, using rework with whichever plugins you'd like will require you to fork this repo and edit your own JS.
-
-### CLI
-
-You can enter stuff into the watch process. Here are some commands:
-
-- `update` - update all pinned dependencies
-- `pin` - update and pin all dependencies (including semver ranges)
-- `outdated` - check for outdated dependencies
-- `resolve` - resolve and build
-- `scripts` - build the scripts
-- `styles` - build the styles
-- `files` - copy/symlink the files
-
-If you just want to build everything, just add a `--kill` option when you start it:
-
-```
-node --harmony-generators ./node_modules/component-watcher --kill
-```
-
-### Testing
-
-```
-npm i
-npm start
-```
-
-Then touch all the local components in `test/` like your spouse.
-
-Please don't pin and update components in the `test/` directory!
+Emitter when a `styles` file has changed. Meaning you can use your previous `tree` and rebuild.
 
 ## License
 
---
+++ test/index.js
@@ -0,0 +1,221 @@
+
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+
+var watch = require('..');
+
+var resolve = path.resolve;
+var root = __dirname + '/fixture';
+
+var options = {
+  root: root
+};
+
+before(function (done) {
+  mkdirp(root + '/lib/reset', done);
+});
+
+beforeEach(function (done) {
+  setTimeout(done, 10);
+});
+
+after(function (done) {
+  rimraf(root + '/lib/reset', done);
+});
+
+describe('Component Watcher', function () {
+  it('should emit "resolve" on initiation', function (done) {
+    var watcher = watch(options);
+
+    watcher.on('resolve', function () {
+      setImmediate(watcher.close)
+      done();
+    });
+  })
+
+  describe('when component.json is touched', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options);
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.utimes(resolve(root, 'component.json'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when index.js is touched', function () {
+    it('should emit "scripts"', function (done) {
+      var watcher = watch(options);
+
+      watcher.once('resolve', function () {
+        watcher.once('scripts', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('resolve', fail);
+        watcher.on('styles', fail);
+
+        fs.utimes(resolve(root, 'index.js'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when index.css is touched', function () {
+    it('should emit "styles"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('styles', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('resolve', fail);
+        watcher.on('scripts', fail);
+
+        fs.utimes(resolve(root, 'index.css'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when boot/component.json is touched', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.utimes(resolve(root, 'lib/boot/component.json'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when boot/index.js is touched', function () {
+    it('should emit "scripts"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('scripts', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('styles', fail);
+        watcher.on('resolve', fail);
+
+        fs.utimes(resolve(root, 'lib/boot/index.js'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when boot/index.css is touched', function () {
+    it('should emit "styles"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('styles', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('resolve', fail);
+
+        fs.utimes(resolve(root, 'lib/boot/index.css'), new Date(), new Date());
+      })
+    })
+  })
+
+  describe('when reset/component.json is created', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.writeFileSync(resolve(root, 'lib/reset/component.json'), '');
+      })
+    })
+  })
+
+  describe('when reset/index.css is created', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.writeFileSync(resolve(root, 'lib/reset/index.css'));
+      })
+    })
+  })
+
+  describe('when reset/component.json is deleted', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.unlinkSync(resolve(root, 'lib/reset/component.json'));
+      })
+    })
+  })
+
+  describe('when reset/index.css is deleted', function () {
+    it('should emit "resolve"', function (done) {
+      var watcher = watch(options)
+
+      watcher.once('resolve', function () {
+        watcher.once('resolve', function () {
+          setImmediate(watcher.close)
+          done();
+        })
+
+        watcher.on('scripts', fail);
+        watcher.on('styles', fail);
+
+        fs.unlinkSync(resolve(root, 'lib/reset/index.css'));
+      })
+    })
+  })
+})
+
+function fail() {
+  throw new Error('nope');
+}
